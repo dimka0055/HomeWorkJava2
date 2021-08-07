01f7e3da437bec035f6cc8262aedda42ee9ae811
dimka0055 <85841124+dimka0055@users.noreply.github.com>
2021-08-07 15:54:17 +0300

    HomeworkJava1.1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentbalance = 2_000_000_000;
+        int remittance = 500_000_000;
+        int total = currentbalance + remittance;
+        System.out.println(total);
+    }
+}
+
